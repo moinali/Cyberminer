cb5f7f9e64f0e329aa7fb698a408ee8e17b4266c
moinali <35587007+moinali@users.noreply.github.com>
2022-07-14 00:22:16 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
+let query = document.querySelector('.query');
+let searchBtn = document.querySelector('.searchBtn');
+//var data = require('./bbc_news.json');
+//import data from './bbcnews.json';
 const userCardTemplate = document.querySelector("[data-user-template]")
 const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
 let users = []
-
 searchInput.addEventListener("input", e => {
   const value = e.target.value.toLowerCase()
   users.forEach(user => {
@@ -14,16 +17,24 @@ searchInput.addEventListener("input", e => {
   })
 })
 
-fetch("https://jsonplaceholder.typicode.com/users")
+fetch("https://moinali.github.io/Cyberminer/index.json")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
       const card = userCardTemplate.content.cloneNode(true).children[0]
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
-      header.textContent = user.name
-      body.textContent = user.email
+      header.textContent = user.title
+      body.textContent = user.link
       userCardContainer.append(card)
-      return { name: user.name, email: user.email, element: card }
+      return { name: user.title, email: user.link, element: card }
     })
   })
+
+searchBtn.onclick = function (){
+  console.log("Hello world!");
+  let url ="https://www.google.com/search?q=" + query.value;
+  window.open (url,'_self');
+  win.focus();
+}
+
