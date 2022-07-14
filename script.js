24515362e903c37656416f56f39f1cbb9b6eb938
moinali <35587007+moinali@users.noreply.github.com>
2022-07-14 16:38:36 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,40 +1,31 @@
 let query = document.querySelector('.query');
 let searchBtn = document.querySelector('.searchBtn');
-//var data = require('./bbc_news.json');
-//import data from './bbcnews.json';
-const userCardTemplate = document.querySelector("[data-user-template]")
-const userCardContainer = document.querySelector("[data-user-cards-container]")
-const searchInput = document.querySelector("[data-search]")
+var fileData = "";
+var result = "";
 
-let users = []
-searchInput.addEventListener("input", e => {
-  const value = e.target.value.toLowerCase()
-  users.forEach(user => {
-    const isVisible =
-      user.name.toLowerCase().includes(value) ||
-      user.email.toLowerCase().includes(value)
-    user.element.classList.toggle("hide", !isVisible)
-  })
-})
 
 fetch("https://moinali.github.io/Cyberminer/index.json")
   .then(res => res.json())
   .then(data => {
-    users = data.map(user => {
-      const card = userCardTemplate.content.cloneNode(true).children[0]
-      const header = card.querySelector("[data-header]")
-      const body = card.querySelector("[data-body]")
-      header.textContent = user.title
-      body.textContent = user.link
-      userCardContainer.append(card)
-      return { name: user.title, email: user.link, element: card }
-    })
+    fileData=data;
   })
 
 searchBtn.onclick = function (){
-  console.log("Hello world!");
-  let url ="https://www.google.com/search?q=" + query.value;
-  window.open (url,'_self');
-  win.focus();
-}
+  result = "";
+  console.log(query.value);
+  for (let i = 0; i < fileData.length; i++) {
+    if(fileData[i].title.includes(query.value) 
+      || fileData[i].description.includes(query.value)) {
+        //create link like so
+        //<a href="https://www.w3schools.com/html/">Visit our HTML Tutorial</a>
+        result = result +"<a href=\""+ fileData[i].link +"\">"+ fileData[i].title +"</a><br>";
+      }
+  }
+  console.log(result.length);
+  if(result.length==0){
+    console.log("here");
+    result = "No results found for " + query.value;
+  }
+  document.getElementById("content").innerHTML = result;
+  }
 
