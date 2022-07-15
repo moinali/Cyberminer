8af23ba1a1aca58c62aa8fd318dadb3fed44ac02
moinali <35587007+moinali@users.noreply.github.com>
2022-07-15 02:28:54 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ fetch("https://moinali.github.io/Cyberminer/index.json")
     fileData=data;
   })
 
+
 searchBtn.onclick = function (){
   result = "";
   console.log(query.value);
@@ -19,9 +20,12 @@ searchBtn.onclick = function (){
         //create link like so
         //<p>This text is normal.</p>
         //<a href="https://www.w3schools.com/html/">Visit our HTML Tutorial</a>
-        result = result + "<p>"+ fileData[i].pubDate.slice(0,-12) +"</p>"; 
-        result = result +"<a href=\""+ fileData[i].link +"\">"+ fileData[i].title +"</a>";
-        result = result +"<p>"+ fileData[i].description +"...</p><br><br>";
+
+        //result = result + "<p>"+ boldQuery() +"</p>"; 
+        //result = result + "<p class="+"\"text-muted\">"+ fileData[i].link +"</p>"; 
+        result = result + "<p style=\"color:#838383\">"+ fileData[i].link +"</p>"; 
+        result = result +"<a href=\""+ fileData[i].link +"\">"+  boldQuery(fileData[i].title) +"</a>";
+        result = result +"<p>"+ fileData[i].pubDate.slice(0,-12) +" — " +boldQuery(fileData[i].description) +"...</p><br><br>";
       }
   }
   console.log(result.length);
@@ -31,4 +35,22 @@ searchBtn.onclick = function (){
   }
   document.getElementById("content").innerHTML = result;
   }
+
+  function boldQuery(line){
+    var lineToWords = line.split(" ");
+    var formed = "";
+    //var str = "\\b"+ query.value +"\\b";
+    //console.log(str);
+    for(var j = 0; j<lineToWords.length; j++){
+      if(new RegExp("\\b"+query.value+"\\b").test(lineToWords[j])){
+      //if(query.value.includes(lineToWords[j])){       
+        formed += "<b>"+lineToWords[j]+"</b>"+" ";
+      }else{
+        formed += lineToWords[j]+" ";
+     }
+    }
+    return formed;
+  }
+
+
 
