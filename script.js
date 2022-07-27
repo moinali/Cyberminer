0231cf53e42cf600889f4234bcc431d2f812923f
moinali <35587007+moinali@users.noreply.github.com>
2022-07-27 18:33:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -38,6 +38,51 @@ let isNot = false;
     document.getElementById("content").innerHTML = result;
   }
 
+  function getAndResults(key, key2){
+    console.log(key + " AND " + key2);
+    //queryDecypher();
+    for (let i = 0; i < fileData.length; i++) {
+      if( (fileData[i].title.includes(key) || fileData[i].description.includes(key)) 
+        && (fileData[i].title.includes(key2) || fileData[i].description.includes(key2))  )
+         {
+          result = result + "<p style=\"color:#838383\">"+ fileData[i].link +"</p>";
+          var boldTitleKey2 = boldQuery(fileData[i].title, key2);
+          var boldDescKey2 = boldQuery(fileData[i].description, key2);
+          result = result +"<a href=\""+ fileData[i].link +"\">"+  boldQuery(boldTitleKey2, key) +"</a>";
+          result = result +"<p>"+ fileData[i].pubDate.slice(0,-12) +" — " +boldQuery(boldDescKey2, key) +"...</p><br><br>";
+        }
+    }
+    console.log(result.length);
+    if(result.length==0){
+      console.log("here");
+      result = "No results found for " + key;
+    }
+    document.getElementById("content").innerHTML = result;
+  }
+
+  function getNotResults(key, key2){
+    console.log(key + " AND " + key2);
+    //queryDecypher();
+    for (let i = 0; i < fileData.length; i++) {
+      if( (fileData[i].title.includes(key) || fileData[i].description.includes(key)) ){
+        if ( !(fileData[i].title.includes(key2) || fileData[i].description.includes(key2)) ) {
+          result = result + "<p style=\"color:#838383\">"+ fileData[i].link +"</p>";
+          var boldTitleKey2 = boldQuery(fileData[i].title, key2);
+          var boldDescKey2 = boldQuery(fileData[i].description, key2);
+          result = result +"<a href=\""+ fileData[i].link +"\">"+  boldQuery(boldTitleKey2, key) +"</a>";
+          result = result +"<p>"+ fileData[i].pubDate.slice(0,-12) +" — " +boldQuery(boldDescKey2, key) +"...</p><br><br>";
+        
+        }
+      }
+    }
+    console.log(result.length);
+    if(result.length==0){
+      console.log("here");
+      result = "No results found for " + key;
+    }
+    document.getElementById("content").innerHTML = result;
+  }
+
   function queryDecypher(){
     var q = query.value;
     var qArr = [];
@@ -45,6 +90,7 @@ let isNot = false;
     if (q.includes("AND") ) {
       qArr = q.split(' AND ');
       console.log("found the AND");
+      getAndResults(qArr[0],qArr[1]);
 
     } else if (q.includes("OR")){ //parse string with OR and list result
       qArr = q.split(' OR ');
@@ -61,12 +107,13 @@ let isNot = false;
     } else if(q.includes("NOT")){ 
       qArr = q.split(' NOT ');
       console.log("found the NOT");
+      getNotResults(qArr[0],qArr[1]);
 
     } else {//single word query
       qArr[0] = q;
       getResults(qArr[0]);
     }
-      
+
   }
   
   function boldQuery(line, key){
